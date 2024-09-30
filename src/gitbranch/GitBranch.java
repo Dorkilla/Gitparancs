99aa6fa69b6eddbfdb4df51f0b1fd42963bae9ba
Dorkilla <dorkilla58@gmail.com>
2024-09-30 13:27:36 +0200

    olvashato kod

--- src/gitbranch/GitBranch.java
+++ src/gitbranch/GitBranch.java
@@ -9,10 +9,10 @@ public class GitBranch {
 
    
     public static void main(String[] args) {
-        fv();
+        int i = GitBranch.fv();
     }
 
-    private static void fv() {
+    private static int fv() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     

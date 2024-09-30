0af47ebacb8c131b588bac5f5430f0be7601397e
Dorkilla <dorkilla58@gmail.com>
2024-09-30 13:20:08 +0200

    alap

---
+++ src/gitbranch/GitBranch.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gitbranch;
+
+/**
+ *
+ * @author kocsi.dorottya
+ */
+public class GitBranch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        in i = fv();
+    }
+
+    private static in fv() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
